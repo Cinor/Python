762346d4189a9a693dea8088e58a9ecaa0932ae7
EN8859 <EN8859@SKEN8859>
2018-05-30 17:47:57 +0800

    pull

--- Demo.py
+++ Demo.py
@@ -3,7 +3,9 @@ import csv
 from bs4 import BeautifulSoup
 
 # 下載 Yahoo 首頁內容
-#r = requests.get('https://tw.yahoo.com/')
+# r = requests.get('https://tw.yahoo.com/')
+
+r = requests.get('https://tw.appledaily.com/new/realtime')
 
 #Google 搜尋 URL
 google_url = 'https://www.google.com.tw/search'
@@ -12,7 +14,7 @@ google_url = 'https://www.google.com.tw/search'
 my_params = {'q': '李登輝'}
 
 # 下載 Google 搜尋結果
-r = requests.get(google_url, params = my_params)
+#r = requests.get(google_url, params = my_params)
 
   # 開啟輸出的 CSV 檔案
 with open('output_test.csv', 'w', newline='') as csvfile:
@@ -27,10 +29,13 @@ with open('output_test.csv', 'w', newline='') as csvfile:
     # 以 BeautifulSoup 解析 HTML 程式碼
     soup = BeautifulSoup(r.text, 'html.parser')
 
+    # 觀察 HTML 原始碼
+    #print(soup.prettify())
+
     # 以 CSS 的 class 抓出各類頭條新聞
     stories = soup.select('div.g > h3.r > a[href^="/url"]')
-    #stories = soup.find_all('a', class_='story-title')
-  # 以 CSS 的 class 抓出各類頭條新聞
+    # stories = soup.find_all('a', class_='story-title')
+    # 以 CSS 的 class 抓出各類頭條新聞
     for s in stories:
 
       # 新聞標題 新聞網址
